b34aa7792bdf879bccbcbf75cfb90b7fd300c981
Vince Carey <stvjc@channing.harvard.edu>
2015-03-26 03:00:20 +0000

    as prep.cisAssocNB for temp evaluation
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/gQTLstats@101197 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/cisAssocPlus.R
+++ R/cisAssocPlus.R
@@ -219,3 +219,174 @@ cisAssoc = function( summex, vcf.tf, rhs=~1, nperm=3, cisradius=50000,
     sapply(split(ex, gt), length)
 }
 
+prep.cisAssocNB = function( summex, vcf.tf, geneind=1, snpind=1, rhs=~1, nperm=3, cisradius=50000, 
+    genome="hg19", assayind="counts", lbmaf=1e-6, dropUnivHet=TRUE,
+    infoFields = c("LDAF", "SVTYPE") ) {
+#
+#
+# strategy is to a) select a gene for cis identification of SNP
+#                b) select a SNP in this set for analysis
+#                c) use all genes for variance estimation
+#
+# LDAF is concession to bug in readVcf where specifying only SVTYPE
+# leads to error on 1KG VCF data
+#
+ #
+ # take all features from SummarizedExperiment
+ # harmonize samples between summex and vcf.tf (TabixFile)
+ # obtain genotypes of variants cis to features in summex -- using only SNVs!!
+ # compute associations between stx(features) and vtx(genotypes)
+ # store in a GRanges ordered by variant address with relevant metadata
+ #
+ # obtain sample IDs and harmonize genotypes and molec phenotype assay data
+ #
+ ng = length(rowRanges(summex))
+ usn = unique(seqnames(summex))
+ if(length(usn)>1) stop("current implementation insists that length(unique(seqnames(summex)))==1 as VCF assumed chr-specific")
+ sampidsInSumm = colnames(summex)
+ sampidsInVCF = sampsInVCF(vcf.tf)
+ oksamp = intersect(sampidsInSumm, sampidsInVCF)
+ stopifnot(length(oksamp)>0)
+ summex = summex[, oksamp]
+ #
+ # harmonize annotation for seqnames -- could use style methods here
+ #
+ sn = force(as.character(seqnames(summex)))
+ stopifnot(length(ctouse <- unique(sn))==1)
+ #
+ # generate cis search space for assay probes
+ #
+ cisr = rowRanges(summex)[geneind]+cisradius
+ seqlevels(cisr) = force(seqlevels(cisr)) # must use VCF-oriented seqlevels
+ #
+ # first pass at genotype data retrieval
+ #
+ vp = ScanVcfParam(fixed="ALT", info=infoFields, geno="GT", 
+     samples=oksamp, which=cisr)  # which will sort variants into groups defined by probes
+ vdata = readVcf(vcf.tf, genome=genome, param=vp) # compressed
+ #
+ # retain only SNVs with MAF > lbmaf
+ # 12/26/2014 -- the exclusion of non-SNVs has become complex
+ # we introduce attempt to capture SVTYPE field above.  this may need
+ # to be moved up to interface
+ svinfo = info(vdata)$SVTYPE
+ if (length(svinfo)>0) {
+   ok = which(is.na(svinfo))
+   vdata = vdata[ok,]
+#
+# but the example extract has an ALT entry of <DEL> for which SVTYPE is NA
+#
+   ael = elementLengths(alt(vdata))
+   vdata = vdata[ which(ael==1), ]
+   stopifnot(length(alt(vdata)) == length(unlist(alt(vdata))))
+   todrop = which(!(unlist(alt(vdata)) %in% c("A", "C", "T", "G")))
+   if (length(todrop)>0) vdata = vdata[-todrop,]
+   tmpalt = try( DNAStringSetList(alt(vdata)) )
+   if (inherits( tmpalt, "try-error" )) stop("attempt to reclass ALT fails after SV exclusion")
+   alt(vdata) = tmpalt
+   }
+ nonSNV = which(!isSNV(vdata))
+ if (length(nonSNV)>0) vdata = vdata[-nonSNV,]
+ gtdata = genotypeToSnpMatrix(vdata)
+ uhetinds = NULL
+ if (dropUnivHet) {
+    message("checking for universal heterozygous loci for exclusion (as dropUnivHet == TRUE) ...")
+    gtchar = as(gtdata[[1]],"character")  # could be slow
+    uhetinds = which(apply(gtchar,2, function(x) all(x %in% c("A/B", "NA"))))
+    if ((nu <- length(uhetinds))>0) 
+      warning(paste0("found ", nu, " universally heterozygous loci."))
+    message("done checking.")
+    }
+ csumm = col.summary(gtdata[[1]])
+ inmafs = csumm[,"MAF"]
+ bad = union(which(inmafs<lbmaf), uhetinds)
+ if (length(bad)>0) {
+   vdata = vdata[-bad,]
+   gtdata = genotypeToSnpMatrix(vdata)
+   }
+ varrd = rowRanges(vdata)  # would like to use this as the backbone of test result report
+# rad = rowRanges(vdata[snpind])+cisradius
+# fo = findOverlaps( rowRanges(summex), rad )
+# stopifnot(length(subjectHits(fo))>0)
+# summex = summex[ subjectHits(fo), ]
+ require(DESeq2)
+ colData(summex)$snp = as(gtdata[[1]], "numeric")[,snpind]
+ metadata(colData(summex))$snpgrange = varrd[snpind]
+ #list(gtdata=gtdata[[1]][,snpind], varrd=varrd[snpind], summex=summex)
+ summex
+}
+
+bag = function() {
+ #
+ # use a list mapping probes to SNVs in cis to organize the testing
+ #
+ uo = unique(varrd$paramRangeID)
+ vdl = split(varrd, varrd$paramRangeID)[uo] # will reorder without uo
+ snpbyprobe = sapply(vdl, names)
+ probes2test = names(snpbyprobe)
+ numdata = assays(summex)[[assayind]][probes2test,]
+ #
+ # force the formula to have form ex~[rhs]
+ #
+ infmla = as.formula(paste("ex", paste(as.character(rhs), collapse="")))
+ tsts = vector("list", length(probes2test) )
+ summs = col.summary(gtdata$genotypes)
+ mafs = summs[,"MAF"]
+ names(mafs) = rownames(summs)
+ #
+ # loop over cis map to collect tests
+ #
+ if (!exists(".Random.seed")) .xyzzy = runif(1)
+ iniSeed = .Random.seed
+ suppressWarnings({
+ for (i in 1:length(probes2test)) {
+   ex = numdata[ probes2test[i], ]
+   tsts[[i]] = snp.rhs.tests( formula=infmla, 
+           snp.data=gtdata$genotypes[, snpbyprobe[[ probes2test[i] ]] ], family="gaussian",
+           data=data.frame(ex=ex, as(colData(summex), "data.frame") ), uncertain=TRUE )
+   }
+  })
+ #
+ # test under permutation for plug-in FDR
+ #
+ perms = vector("list", nperm)
+ permit = function(x) x[sample(1:length(x), size=length(x), replace=FALSE)]
+ for (j in 1:nperm) {
+   perms[[j]] = vector("list", length(probes2test))
+   suppressWarnings({
+   for (i in 1:length(probes2test)) {
+     ex = permit(as.numeric(numdata[ probes2test[i], ]))
+     perms[[j]][[i]] = snp.rhs.tests( formula=infmla, 
+             snp.data=gtdata$genotypes[, snpbyprobe[[ probes2test[i] ]] ], family="gaussian",
+             data=data.frame(ex=ex, as(colData(summex), "data.frame") ), uncertain=TRUE )
+     }
+    })
+   }
+ names(tsts) = probes2test
+ #
+ # bind test results to varrd GRanges instance -- note that ALT may be either
+ # CharacterList or DNAStringSetList and this may need attention on collection
+ #
+ chisqs = unlist(lapply(tsts, chi.squared))
+ varrd$chisq = chisqs
+ pnames = paste0("permScore_", 1:nperm)
+ for (i in 1:nperm) {
+   mcols(varrd)[ ,pnames[i] ] = unlist(lapply(perms[[i]], chi.squared))
+   }
+ varrd$snp = names(varrd)
+ varrd$MAF = as.numeric(mafs[varrd$snp])
+ varrd$probeid = as.character(varrd$paramRangeID)
+ metadata(varrd)$sessInfo = sessionInfo()
+ metadata(varrd)$init.Random.seed = iniSeed
+ names(varrd) = NULL
+ 
+ snpl = start(varrd)
+ gstart = abs(start(summex[varrd$probeid,]))
+ gend = abs(end(summex[varrd$probeid,]))
+
+ dists = pmin(abs(snpl-gstart), abs(snpl-gend))
+ dists[ which((snpl >= gstart) & (snpl <= gend))] = 0
+ varrd$mindist = dists
+
+ varrd
+}
