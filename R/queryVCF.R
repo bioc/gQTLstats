e05a6e3026e0816d075d765a78e0f836ae87792b
Vince Carey <stvjc@channing.harvard.edu>
2016-03-14 19:37:55 +0000

    various upgrades including gQTLs for managing filtered stores
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/gQTLstats@114743 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/queryVCF.R
+++ R/queryVCF.R
@@ -1,5 +1,5 @@
 
-queryVCF = function (gr, vcf.tf, samps, genome = "hg19", getSM=TRUE) 
+queryVCF = function (gr, vcf.tf, samps, genome = "hg19", getSM=TRUE, snvOnly=TRUE) 
 {
 #
 # support anonymous extraction of VCF/SnpMatrix by range
@@ -16,6 +16,7 @@ queryVCF = function (gr, vcf.tf, samps, genome = "hg19", getSM=TRUE)
       stopifnot(length(int)>0)
       }
     readout = readVcf(vcf.tf, genome = genome, param = vp)
+    if (snvOnly) readout = readout[which(isSNV(readout)),]
     sm = NULL
     if (getSM) sm = genotypeToSnpMatrix(readout)
     list(readout=readout, sm=sm)
@@ -34,8 +35,37 @@ prepEqData = function (gene, se, tf, snpgr, genome = "hg19",
     okids = intersect(esamps, rownames(smat))
     ex = assay(se[gene, okids])
     gt = as.character(as(smat[okids,], "character"))
-    list(ex=ex, gt=gt, coln=colnames(smat))
+    list(ex=ex, gt=gt, coln=colnames(smat), sampids=okids)
 }
+
+gQTLs = function(filtgr, se, tf, genome="hg19", forceRs=TRUE, chunksize=50) {
+    # purpose is to generate a SummarizedExperiment from filtered ciseStore
+    # filtgr is GRanges from filtering ciseStore, tf is chromosome-specific VCF so
+    # be sure that filtgr is properly restricted
+    thecall = match.call()
+    stopifnot(all(c("probeid", "snp") %in% names(mcols(filtgr))))
+    vh = scanVcfHeader(tf)
+    vcfids = vcfSamples(vh)
+    okids = intersect(vcfids, colnames(se))
+    stopifnot(length(okids)>0)
+    ex = assay(se[ filtgr$probeid, okids ])
+    rownames(ex) = NULL
+    chunks = chunk(1:length(filtgr), chunk.size=chunksize)
+    snpd = foreach (ch = chunks) %dopar% {
+          q1 = queryVCF(gr=filtgr[ch], vcf.tf=tf, samps=okids,
+               genome=genome, getSM=TRUE)$sm[[1]] 
+          t(as(q1[okids,], "character"))
+          }
+    snpd = do.call(rbind, snpd) #
+#    rownames(snpd) = NULL
+    se = SummarizedExperiment(List(calls=snpd, exprs=ex), colData=colData(se)[colnames(ex),])
+    names(filtgr) = filtgr$snp
+    rowRanges(se) = filtgr
+    metadata(se)$thecall = thecall
+    metadata(se)$tfpath = path(tf)
+    se
+}
+  
 
 eqBox2 = function (gene, se, tf, snpgr, genome = "hg19", forceRs = TRUE,
     ...) {
@@ -47,4 +77,62 @@ eqDesc2 = function (gene, se, tf, snpgr, genome = "hg19", forceRs=TRUE) {
     ans = prepEqData( gene=gene, se=se, tf=tf, snpgr=snpgr, genome=genome, forceRs=forceRs )
     sapply(split(ans$ex, ans$gt),length)
 }
+
+setClass("SnpToGeneQTL", representation(exprs="numeric",
+   genotypes="ANY", gene="character", snpid="character",
+   snpgr="GRanges", genome="character", boxdata="ANY",
+   covars="DataFrame"))
+setGeneric("SnpToGeneQTL", function(gene, snpid, snpgr, tf, se, genome, forceRs) 
+   standardGeneric("SnpToGeneQTL"))
+
+setMethod("SnpToGeneQTL", c(gene="character", snpid="character", snpgr="GRanges",
+     tf="TabixFile", se="SummarizedExperiment"), function(gene, snpid, snpgr, tf, se,
+         genome="hg19", forceRs=TRUE) {
+    ans = prepEqData( gene, se, tf, snpgr, genome, forceRs=forceRs )
+    ab = boxplot(split(ans$ex, ans$gt), plot=FALSE)
+    new("SnpToGeneQTL", exprs=as.numeric(ans$ex), genotypes=ans$gt, gene=gene, snpgr=snpgr, genome=genome, boxdata=
+       ab, covars=colData(se)[colnames(ans$ex),], snpid=snpid)
+})
+
+setMethod("SnpToGeneQTL", c(gene="missing", snpid="missing", snpgr="GRanges",
+     tf="TabixFile", se="SummarizedExperiment"), function(gene, snpid, snpgr, tf, se,
+         genome="hg19", forceRs=TRUE) {
+    snpid = mcols(snpgr)$snp
+    gene = mcols(snpgr)$probeid
+    ans = prepEqData( gene, se, tf, snpgr, genome, forceRs=forceRs )
+    ab = boxplot(split(ans$ex, ans$gt), plot=FALSE)
+    new("SnpToGeneQTL", exprs=as.numeric(ans$ex), genotypes=ans$gt, gene=gene, snpgr=snpgr, genome=genome, boxdata=
+       ab, covars=colData(se)[colnames(ans$ex),], snpid=snpid)
+})
+
+
+
+
+setMethod("show", "SnpToGeneQTL", function(object){
+cat("gQTLstats SnpToGeneQTL instance:\n")
+cat("SNP ", object@snpid, "; gene: ", object@gene, "\n", sep="")
+cat("use boxswarm() for visualization\n")
+})
+setGeneric("boxswarm", function(x, colvar, colmap, inxlab, inylab, ...)
+     standardGeneric("boxswarm"))
+setMethod("boxswarm", "SnpToGeneQTL", function(x, colvar, colmap, inxlab, inylab, ...) {
+ if (!missing(colvar)) {
+     cov2use = x@covars[[colvar]]
+     collist = split(cov2use, x@genotypes)
+     ucov = unique(cov2use)
+     if (missing(colmap)) {
+        colmap = 1:length(ucov)
+        names(colmap) = ucov
+        }
+     pwcols = lapply(collist, function(x) colmap[x])
+     }
+ else pwcols=NULL
+ if (missing(inylab)) inylab=g1@gene
+ if (missing(inxlab)) inxlab=g1@snpid
+ beeswarm(split(x@exprs, x@genotypes), pwcol=pwcols, pch=19, xlab=inxlab, ylab=inylab, ...)
+ bd = x@boxdata
+ bd$out=NULL
+ bd$group=NULL
+ bxp(bd, add=TRUE, boxfill="transparent")
+})
 
