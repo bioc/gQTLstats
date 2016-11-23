68d19b03e23da155ba0b5bb24ea581343fc86e28
Vince Carey <stvjc@channing.harvard.edu>
2016-11-23 17:02:29 +0000

    label chromatin states in manh using erma
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/gQTLstats@124413 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/tqbrowser.R
+++ R/tqbrowser.R
@@ -8,9 +8,12 @@ band2feats = function(cbstruct, bandid, gr, featExtractor = function(x)names(x))
  }
 
 tqbrowser = function( mae, felname, gelname, tiling, tsbra, annovec,
-  band.init="6q12",  ...) {
+  band.init="6q12", ermaset=NULL, ...) {
 # 
 # browse ranked trans-associations in tiles
+#
+# optional ermaset will supply information on cell-type
+# specific chromatin states of genomic intervals
 #
 # mae has feature data, felname picks feature component,
 # gelname picks genotypes component, tiling is a GRanges,
@@ -33,6 +36,7 @@ tqbrowser = function( mae, felname, gelname, tiling, tsbra, annovec,
        selectInput("curband", "cytoband", choices=names(tiling),
           selected=band.init),
        uiOutput("snpSelector"),
+       uiOutput("celltypeSelector"),
        numericInput("rank", "rank", 1, min=1, max=5, step=1),
        numericInput("num2lab", "# to label", 5, min=5, max=50, step=5),
        width=3
@@ -56,6 +60,13 @@ tqbrowser = function( mae, felname, gelname, tiling, tsbra, annovec,
          band2feats(tiling, input$curband, tsbra))
        )
       })
+   if (!is.null(ermaset)) 
+    output$celltypeSelector = renderUI({
+     tagList(
+       selectInput("celltype", "celltype", choices=
+         cellTypes(ermaset), selected=cellTypes(ermaset)[4])
+       )
+      })
    output$manh = renderPlot({
       x = band2feats(tiling, input$curband, tsbra, function(x) start(x))
       y = band2feats(tiling, input$curband, tsbra, function(x) 
@@ -66,15 +77,32 @@ tqbrowser = function( mae, felname, gelname, tiling, tsbra, annovec,
       text(x[topinds], jitter(y[topinds]), nm[topinds], cex=1.1)
       })
    output$manh3 = renderPlotly({
+    statevec = NULL
+    if (!is.null(ermaset)) {
+      curr = tiling[input$curband]
+      print(curr)     
+      seqlevelsStyle(curr) = "UCSC" # match ermaset
+      rowRanges(ermaset) = curr
+print(input$celltype)
+      ind = which(cellTypes(ermaset) == input$celltype)
+print(ind)
+      curstates = subsetByRanges(ermaset[,ind], curr)[[1]][[1]] #multiple files, multiple ranges permitted, we are using 1,1
+ #     print(curstates)     
+      seqlevelsStyle(curstates) = seqlevelsStyle(tsbra)
+      fo = findOverlaps(subsetByOverlaps(tsbra, tiling[input$curband]), curstates)
+      print(fo)
+      statevec = curstates$name[ subjectHits(fo) ]
+      }
       x = band2feats(tiling, input$curband, tsbra, function(x) start(x))
       y = band2feats(tiling, input$curband, tsbra, function(x) 
             x$allscores[,input$rank])
       nm = band2feats(tiling, input$curband, tsbra, function(x) names(x))
-      topinds = order(y,decreasing=TRUE)[1:input$num2lab]
+#      topinds = order(y,decreasing=TRUE)[1:input$num2lab]
 #      plot(x,y,xlab=input$curband,cex.lab=1.2)
 #      text(x[topinds], jitter(y[topinds]), nm[topinds], cex=1.1)
-      curdf = data.frame(pos=x, assoc=y, snp=nm, stringsAsFactors=FALSE)
-      ggplotly(ggplot(curdf, aes(x=pos, y=assoc, text=snp)) + geom_point())
+      curdf = data.frame(pos=x, assoc=y, snp=nm, state=paste0(nm, ":", statevec), stringsAsFactors=FALSE)
+      pp = ggplot(curdf, aes(x=pos, y=assoc, text=state)) + geom_point()
+      ggplotly(pp)
       })
    tt = reactive({
 #
